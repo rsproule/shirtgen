0c3a2212cc152a841e3387bed40d598f84e0ce08
Ryan Sproule <rsproule23@gmail.com>
2025-08-01 17:40:47 -0400

    shirt location + progressive load images

--- src/components/3d/Scene3D.tsx
+++ src/components/3d/Scene3D.tsx
@@ -10,13 +10,14 @@ export function Scene3D({ children }: Scene3DProps) {
   return (
     <div className="flex-1 bg-gray-50 w-full h-full">
       <Canvas className="w-full h-full">
-        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 6]} />
         <OrbitControls
           enablePan={true}
           enableZoom={true}
           enableRotate={true}
-          minDistance={0.5}
-          maxDistance={8}
+          minDistance={2}
+          maxDistance={10}
+          target={[0, 0, 0]}
         />
         <Environment preset="studio" />
         <ambientLight intensity={0.3} />
