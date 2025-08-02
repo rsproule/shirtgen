7fa5e985422e20a1da4e2eadc7ddd209904caf3c
Brian Sproule <briansproule20@gmail.com>
2025-08-02 14:53:25 -0400

    Apply Comic Sans font to ShirtSlop title and remove demo components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'comic': ['Comic Sans MS', 'Comic Sans', 'Chalkboard SE', 'Comic Neue', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
