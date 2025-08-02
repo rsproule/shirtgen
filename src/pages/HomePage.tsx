c1ed94154d1f43f1896df3467bf8eeb28629b582
Brian Sproule <briansproule20@gmail.com>
2025-08-02 14:35:21 -0400

    Move title alignment to the left

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -93,7 +93,7 @@ export function HomePage() {
       <div className="relative">
         {/* Title */}
         <div className="mx-auto max-w-7xl px-8 pt-8 pb-4 text-left">
-          <div className="flex items-center gap-6 mb-3">
+          <div className="flex items-center justify-start gap-6 mb-3">
             <img 
               src="/shirtslop.png" 
               alt="ShirtSlop Logo" 
