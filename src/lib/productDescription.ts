890119a84b5439315960c70c915493e11a6f7301
Ryan Sproule <rsproule23@gmail.com>
2025-08-03 22:18:33 -0400

    desc

--- src/lib/productDescription.ts
+++ src/lib/productDescription.ts
@@ -1,9 +1,7 @@
 import type { EchoUser } from "@zdql/echo-react-sdk";
 
-export const PRODUCT_DESCRIPTION_TEMPLATE = (creator: EchoUser | null) =>
-  `Created on https://shirtslop.com${
-    creator ? ` by\n\nID: ${creator.name || creator.email}` : ""
-  }
-\n 
-${creator?.id}
+export const PRODUCT_DESCRIPTION_TEMPLATE = (creator: EchoUser | null) => `
+Created on <a href="https://shirtslop.com" target="_blank" rel="noopener noreferrer">shirtslop.com</a>
+${creator ? `<br><br>by: ${creator.name || creator.email}` : ""}
+${creator?.id ? `<br><br><span style="color: #888;">[${creator.id}]</span>` : ""}
 `;
